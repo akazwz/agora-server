f469c21ec41bd47eddd9cae3af3f7afda1b34cbc
akazwz <akazwz@pm.me>
2021-11-10 11:35:10 +0800

    add struct

--- server.go
+++ server.go
@@ -2,8 +2,17 @@ package main
 
 import (
 	"fmt"
+	"log"
 )
 
+type RtcToken struct {
+	UID         uint32 `json:uid`
+	ChannelName string `json:channel_name`
+	Role        uint32 `json:role`
+}
+
 func main() {
+	var token RtcToken
+	log.Println(token)
 	fmt.Println("Hello Agora!")
 }
